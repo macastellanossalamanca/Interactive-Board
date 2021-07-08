9483c9527f04fc39898b96250a82afaab6e58eaf
Miguel Castellanos <miguel.castellanos@escuelaing.edu.co>
2021-07-08 17:12:36 -0500

    Avance

---
+++ src/main/java/edu/escuelaing/InteractiveBoard/Controller/DrawingServiceController.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.InteractiveBoard.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DrawingServiceController {
+
+	@GetMapping("/status")
+	public String status() {
+		return "{\"status\":\"Greetings from Spring Boot. " + java.time.LocalDate.now() + ", "
+				+ java.time.LocalTime.now() + ". " + "The server is Runnig!\"}";
+	}
+}
